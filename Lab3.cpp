40974d888515785cd621f839c6a64aadde14f298
semenckin.e <semenckin.e@yandex.ru>
2020-10-16 21:38:55 +0300

    changed

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,69 +1,69 @@
-//#include "stdafx.h"
+// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include "mpi.h"
-#include "header.h"
+#include "utils.hpp"
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
 
 
-int read_file(std::ifstream& input, int* result);
-int partition(int* A, int lo, int hi);
-void quicksort(int* A, int lo, int hi);
-int lomuto_partition(int* A, int lo, int hi);
-int sort_recursive(int* arr, int arrSize, int currProcRank, int maxRank, int rankIndex);
-void write_file(std::ofstream& output, int arrSize, int* arr);
-void generateArray(const std::string& xsFilename, int rows, double initNum);
+using namespace std;
+using namespace utils;
 
-int main(int argc, char* argv[]) {
-	if (argc == 3) {		
-		generateArray(argv[1], atoi(argv[2]));
-		return 0;
-	}
-	int size, rank;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+int main(int argc, char** argv) {
 
-	
+    int* nums = nullptr;
+    int numsSize = 0;
 
-	if (argc != 4) { std::cout << "Usage: enter input and output filenames, number of iterations to perform sorting.\n"; return 1; }
-	int iter_count = atoi(argv[3]);
-	
-	for (int iter = 0; iter < iter_count; iter++) {
-		
-		int rankPower = 0;
-		while (pow(2, rankPower) <= rank)
-			rankPower++;
+    if (argc == 4) {
+        createFileOfNums(argv[1], atoi(argv[2]), -10000, 10000);
+    }
+    else if (argc != 3) {
+        cout << "Not all arguments specified!" << endl;
+        return (1);
+    }
+    else
+    {
+        string inFile(argv[1]);
+        string outFile(argv[2]);
+        MPI_Init(&argc, &argv);
 
-		if (rank == 0) {
-			auto start = std::chrono::high_resolution_clock::now();
+        int size;
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-			std::ifstream input(argv[1]);
-			if (!input) { std::cout << "Input file not found.\n"; return 1; }
+        if (rank == 0) {
+            tie(nums, numsSize) = loadNums(inFile);
+        }
+        MPI_Bcast(&numsSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        double wtime = MPI_Wtime();
 
-			
-			int* numbers = (int*)malloc(MAX_SIZE * sizeof(int));
-			int arraySize = read_file(input, numbers);
+        if (size == 1) {
+            quicksort(nums, numsSize);
+        }
+        else {
+            quicksortMPI(nums, numsSize, rank, size);
+        }
 
-			sort_recursive(numbers, arraySize, rank, size - 1, rankPower);
+        MPI_Barrier(MPI_COMM_WORLD);
+        wtime = MPI_Wtime() - wtime;
 
-			std::ofstream output(argv[2]);
-			write_file(output, arraySize, numbers);
-			auto finish = std::chrono::high_resolution_clock::now();
-			std::cout << double(std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()) / 1000 << std::endl;
-		}
-		else {
-			MPI_Status status;
-			int subarray_size;
-			MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			
-			MPI_Get_count(&status, MPI_INT, &subarray_size);
-
-			int source_process = status.MPI_SOURCE;
-			int* subarray = (int*)malloc(subarray_size * sizeof(int));
-
-			MPI_Recv(subarray, subarray_size, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			sort_recursive(subarray, subarray_size, rank, size - 1, rankPower);
-			MPI_Send(subarray, subarray_size, MPI_INT, source_process, 0, MPI_COMM_WORLD);
-		}
-	}
-	MPI_Finalize();
-	return 0;
+        if (rank == 0) {
+            char buf[1000];
+            int res = -1;
+            res = snprintf(buf, sizeof(buf), "%d Process: %d Size, %f Time\n", size, numsSize, wtime);
+            string str = "error!";
+            if (res >= 0 && res < sizeof(buf))
+                str = buf;
+            cout << str << endl;
+            writeLogTime(str);
+            printAnswer(nums, numsSize, outFile);
+        }
+        MPI_Finalize();
+    }
+    return 0;
 }
---
+++ utils.hpp
@@ -0,0 +1,230 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+namespace utils {
+	tuple<int*, int> loadNums(const string& filename) {
+		int* nums;
+		int size;
+		ifstream file(filename);
+		if (!file) {
+			cerr << "Error opening file: " << filename << ".\n";
+			MPI_Abort(MPI_COMM_WORLD, 1);
+			throw runtime_error("Could not open the file");
+		}
+		file >> size;
+		nums = new int[size];
+		for (int i = 0; i < size; i++) {
+			file >> nums[i];
+
+		}
+		file.close();
+		return make_tuple(nums, size);
+	}
+
+	void createFileOfNums(const string& filename, int size, int begin, int end) {
+		srand((unsigned)time(0));
+		ofstream fileout(filename, ios_base::trunc);
+		fileout << size << endl;
+		for (int i = 0; i < size; i++) {
+			fileout << (rand() % end) + begin << " ";
+		}
+		fileout.close();
+	}
+
+	void writeLogTime(string& str) {
+		ofstream filelog("log.txt", ios::out | ios::app);
+		filelog << str;
+		filelog.close();
+	}
+
+	void printAnswer(int* nums, int numsSize, const string& outFile) {
+		ofstream fout(outFile, ios_base::trunc);
+		fout << "Size: " << numsSize << std::endl;
+		for (int i = 0; i < numsSize; i++)
+			fout << nums[i] << " ";
+		fout.close();
+	}
+	void findPivotAndSwap(MPI_Comm current_communicator);
+	void gatherSortArray(int* array, int numsProcess);
+	void moveItemsInOrder(int rank, MPI_Comm communicator);
+
+	enum tags {
+		ARRAY,
+		SIZE,
+		INIT,
+		ORDER,
+	};
+
+	int splitPosition = 0;
+	int order = 0;
+	int arrayLocalSize = 0;
+	int* arrayLocal = nullptr;
+
+	int compare(const void* x1, const void* x2)
+	{
+		return (*(int*)x1 - *(int*)x2);
+	}
+
+	void quicksort(int* array, int size) {
+		qsort(array, size, sizeof(int), compare);
+	}
+
+	void quicksortMPI(int* array, int arrayIntialSize, int rank, int numsProcess) {
+		if (rank == 0) {
+			// send initial data
+			int partSize = arrayIntialSize / numsProcess;
+			for (int process = 1; process < numsProcess; process++) {
+				int first = process * partSize;
+				int last = (process == numsProcess - 1 ? arrayIntialSize : first + partSize);
+				int size = last - first;
+				MPI_Send(&size, 1, MPI_INT, process, SIZE, MPI_COMM_WORLD);
+				MPI_Send(array + first, size, MPI_INT, process, INIT, MPI_COMM_WORLD);
+			}
+			// copy initial data
+			int first = rank * partSize;
+			int last = (0 == numsProcess - 1 ? arrayIntialSize : first + partSize);
+			arrayLocalSize = last - first;
+			arrayLocal = new int[arrayLocalSize];
+			memcpy(arrayLocal, array, arrayLocalSize * sizeof(int));
+
+			findPivotAndSwap(MPI_COMM_WORLD);
+			gatherSortArray(array, numsProcess);
+		}
+		else {
+			// recive initial data
+			MPI_Recv(&arrayLocalSize, 1, MPI_INT, 0, SIZE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			arrayLocal = new int[arrayLocalSize];
+			MPI_Recv(arrayLocal, arrayLocalSize, MPI_INT, 0, INIT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			findPivotAndSwap(MPI_COMM_WORLD);
+
+			// send result
+			MPI_Send(&arrayLocalSize, 1, MPI_INT, 0, SIZE, MPI_COMM_WORLD);
+			MPI_Send(&order, 1, MPI_INT, 0, ORDER, MPI_COMM_WORLD);
+			MPI_Gatherv(arrayLocal, arrayLocalSize, MPI_INT, nullptr, nullptr, nullptr, MPI_INT, 0, MPI_COMM_WORLD);
+		}
+	}
+
+	void findPivotAndSwap(MPI_Comm currentCommunicator) {
+
+		int currentSize;
+		int currentRank;
+		MPI_Comm_rank(currentCommunicator, &currentRank);
+		MPI_Comm_size(currentCommunicator, &currentSize);
+		bool mainProcess = currentRank == 0;
+
+		if (currentSize == 1) {
+			quicksort(arrayLocal, arrayLocalSize);
+			return;
+		}
+
+		int pivot;
+		if (mainProcess) {
+			// select pivot
+			std::random_device rd;
+			std::default_random_engine generator(rd());
+			std::uniform_int_distribution<int> distribution(0, arrayLocalSize - 1);
+			int randIndex = distribution(generator);
+			pivot = arrayLocal[randIndex];
+		}
+		MPI_Bcast(&pivot, 1, MPI_INT, 0, currentCommunicator);
+		// rearrange
+		int i, j;
+		for (i = 0, j = arrayLocalSize - 1; i < j;) {
+			while (i < j && arrayLocal[i] < pivot) i++;
+			while (i < j && arrayLocal[j] >= pivot) j--;
+			std::swap(arrayLocal[i], arrayLocal[j]);
+		}
+		splitPosition = i;
+
+		moveItemsInOrder(currentRank, currentCommunicator);
+
+		MPI_Comm newCommunicator;
+		MPI_Comm_split(currentCommunicator, currentRank & 1, 0, &newCommunicator);
+
+		order = 2 * order + (currentRank & 1);
+
+		findPivotAndSwap(newCommunicator);
+	}
+
+	void moveItemsInOrder(int rank, MPI_Comm communicator) {
+		int neighbourDiff = 1;
+		bool high = rank & neighbourDiff;
+		int sendSize;
+		int neighbour;
+		int copyFrom;
+		int copyTo;
+		int sendFrom;
+
+		if (high) {
+			neighbour = rank - neighbourDiff;
+			sendSize = splitPosition;
+			sendFrom = 0;
+			copyFrom = splitPosition;
+			copyTo = arrayLocalSize;
+		}
+		else {
+			neighbour = rank + neighbourDiff;
+			sendSize = arrayLocalSize - splitPosition;
+			sendFrom = splitPosition;
+			copyFrom = 0;
+			copyTo = splitPosition;
+		}
+
+		int recvSize;
+		MPI_Sendrecv(&sendSize, 1, MPI_INT, neighbour, SIZE, &recvSize, 1, MPI_INT, neighbour, SIZE, communicator, MPI_STATUS_IGNORE);
+
+		int sizeTemp = recvSize + arrayLocalSize - sendSize;
+		int* newArray = new int[sizeTemp];
+		MPI_Sendrecv(arrayLocal + sendFrom, sendSize, MPI_INT, neighbour, ARRAY, newArray, recvSize, MPI_INT, neighbour, ARRAY, communicator, MPI_STATUS_IGNORE);
+
+		for (int i = copyFrom; i < copyTo; i++) {
+			int index = i - copyFrom + recvSize;
+			newArray[index] = arrayLocal[i];
+		}
+
+		delete[] arrayLocal;
+		arrayLocal = newArray;
+		arrayLocalSize += recvSize - sendSize;
+	}
+
+	void gatherSortArray(int* array, int numsProcess) {
+
+		int size = numsProcess;
+		int* counts = new int[size];
+		int* displacements = new int[size];
+		int* orders = new int[size];
+		orders[0] = order;
+		counts[0] = arrayLocalSize;
+		displacements[0] = 0;
+
+		for (int process = 1, localSize, processOrder; process < size; process++) {
+			MPI_Recv(&localSize, 1, MPI_INT, process, SIZE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&processOrder, 1, MPI_INT, process, ORDER, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			counts[process] = localSize;
+			orders[process] = processOrder;
+		}
+
+		for (int processOrder = 0, offset = 0; processOrder < size; processOrder++) {
+			for (int process = 0; process < size; process++) {
+				if (orders[process] == processOrder) {
+					displacements[process] = offset;
+					offset += counts[process];
+					break;
+				}
+			}
+		}
+
+		MPI_Gatherv(arrayLocal, arrayLocalSize, MPI_INT, array, counts, displacements, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+}
